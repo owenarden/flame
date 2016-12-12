2c67ee95be64f1c18e5c529c3c882a1be20f0c01
Owen Arden <owenarden@gmail.com>
2016-12-12 15:49:01 -0500

    test case for type applications and unification

--- tests/TestDo.hs
+++ tests/TestDo.hs
@@ -7,13 +7,15 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeOperators, PostfixOperators #-}
 {-# LANGUAGE DataKinds #-}
-{-# OPTIONS_GHC -fplugin Flame.Type.Solver #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -fplugin Flame.Solver #-}
 module TestDo where
 
+import Flame.Runtime.Prelude
 import Prelude hiding (fmap, pure, (<*>), return, (>>=), print, putStr, putStrLn, getLine)
 import qualified Prelude as M (fmap, pure, (<*>), return, (>>=))
 import Debug.Trace
-import System.IO
 import Control.Monad.IO.Class
 import System.Posix.User
 import Data.List
@@ -33,13 +35,13 @@ password = label "password"
 
 {- | Get the password from the client -}
 inputPass :: SPrin client
-            -> IFCHandle (I client)
-            -> IFCHandle (C client)
+            -> FLAHandle (I client)
+            -> FLAHandle (C client)
             -> IFC IO (I Alice) (I Alice) String
 inputPass client stdin stdout = 
   {- Endorse the guess to have Alice's integrity -}
   assume ((client*←) ≽ (alice*←)) $
-    reprotect $ hGetLinex (alice*←) stdin
+    reprotect $ hGetLine @(I Alice) stdin
                          
 {- | Compare a string to the password -}
 chkPass :: SPrin client
@@ -50,27 +52,26 @@ chkPass client guess =
    {- Declassify the comparison with the password -}
    assume ((SBot*←) ≽ (alice*←)) $
    assume ((SBot*→) ≽ (alice*→)) $ do
-   pwd <- liftx (alice*←) password
-   protectx (alice*←) $ 
+   pwd <- lift @_ @_ @_ @_ @_ @(I Alice) password
+   protect @_ @_ @_ @_ @(I Alice) $ 
      if pwd == guess then
        Just $ ((*∇) client ≽ (*∇) alice, client ≽ alice)
      else
        Nothing
   
-secure_main :: DPrin user
-        -> IFCHandle (I user)
-        -> IFCHandle (C user)
+secure_main :: forall user. DPrin user
+        -> FLAHandle (I user)
+        -> FLAHandle (C user)
         -> IFC IO ((C user) ∧ (I Alice)) SU ()
 secure_main userprin stdin stdout =
   let user = (st userprin) in
-  let pc = (user*→) *∧ (alice*←) in
   do pass <- inputPass user stdin stdout
      mdel <- chkPass user pass
      case mdel of
        Just (vdel,del) ->
          {- Use the granted authority print Alice's secret -}
          assume vdel $ assume del $ do
-           secret <- liftx pc aliceSecret
-           hPutStrLnx pc stdout secret
+           secret <- lift @_ @_ @_ @_ @_ @((C user) ∧ (I Alice)) aliceSecret
+           hPutStrLn @((C user) ∧ (I Alice)) stdout secret
        Nothing ->
-         hPutStrLnx pc stdout "Incorrect password."
+         hPutStrLn @((C user) ∧ (I Alice)) stdout "Incorrect password."
