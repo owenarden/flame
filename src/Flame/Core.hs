21631ed13206e0f7eef735769903512290b61583
Owen Arden <owenarden@gmail.com>
2018-10-26 15:34:22 -0700

    support TH do syntax for Flame

---
+++ src/Flame/Core.hs
@@ -0,0 +1,12 @@
+module Flame.Core
+       ( module Flame.IFC
+       , module Flame.Principals
+       , module Flame.Runtime.Principals
+       , module Flame.Do
+       )
+  where
+
+import Flame.IFC
+import Flame.Principals
+import Flame.Runtime.Principals
+
